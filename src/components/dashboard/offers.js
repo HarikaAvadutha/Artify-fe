c4435cd8b2f8468751f694ec7912f2a0a207915c
Dasa Srujan Aditya <srujanaditya@gmail.com>
2021-07-18 02:53:55 +0530

    updated

--- src/components/dashboard/offers.js
+++ src/components/dashboard/offers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'antd';
+import FontAwesome from 'react-fontawesome';
 import { TemplateStyles as TemplateOff } from './style';
 
 export default function Offers() {
@@ -7,13 +8,13 @@ export default function Offers() {
     const offers = [
         {
             name: 'Roger Massey',
-            status: 'consigned',
+            status: 'Consigned',
             daysToSale: 120,
             year: 2021
         },
         {
             name: 'Roger Massey',
-            status: 'sold',
+            status: 'Sold Privately',
             daysToSale: 10,
             year: 2021
         },
@@ -25,19 +26,19 @@ export default function Offers() {
         },
         {
             name: 'Roger Massey',
-            status: 'lost',
+            status: 'Lost',
             daysToSale: 120,
             year: 2021
         },
         {
             name: 'Roger Massey',
-            status: 'consigned',
+            status: 'Consigned',
             daysToSale: 120,
             year: 2020
         },
         {
             name: 'Roger Massey',
-            status: 'sold',
+            status: 'Sold',
             daysToSale: 10,
             year: 2020
         },
@@ -49,7 +50,7 @@ export default function Offers() {
         },
         {
             name: 'Roger Massey',
-            status: 'lost',
+            status: 'Lost',
             daysToSale: 120,
             year: 2020
         }
@@ -71,8 +72,17 @@ export default function Offers() {
                     return (
                         <Row className="tableItems">
                             <Col lg={10} md={10} xs={10}>{item.name}</Col>
-                            <Col lg={6} md={6} xs={6}>{item.status? item.status : 'Inc'}</Col>
-                            <Col lg={8} md={8} xs={8} style={{ textAlign: 'right' }}>{item.daysToSale? item.daysToSale : '-'}</Col>
+                            <Col lg={2} md={2} xs={2} >
+                                {item.status === 'Consigned' && <FontAwesome name="tag" style={{ color: '#00FF87' }} />}
+                                {item.status === 'Sold Privately' && <FontAwesome name="dollar" style={{ color: '#00FF87' }} />}
+                                {item.status === 'Sold' && <FontAwesome name="gavel" style={{ color: '#00FF87' }} />}
+                                {item.status === 'Lost' && <FontAwesome name="thumbs-down" style={{ color: '#FFB928' }} />}
+                                {!item.status && <FontAwesome name="question-circle" style={{ color: '#9598A5' }} />}
+                            </Col>
+                            <Col lg={8} md={8} xs={8} style={{color: !item.status || item.status === 'Lost' ? '#9598A5' : '#00FF87'}}>
+                                {item.status ? item.status : 'Inc'}
+                            </Col>
+                            <Col lg={4} md={4} xs={4} style={{ textAlign: 'right' }}>{item.daysToSale ? item.daysToSale : '-'}</Col>
                         </Row>
                     )
                 })
